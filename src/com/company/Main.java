93ac652ed93aefbdde19e82a9b740d2f79840650
EwaSztomberska <ewa.sztomberska@com>
2020-07-18 13:08:59 +0200

    Zadanie5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int number = scanner.nextInt();
+        for(int i = 2; i < number; i ++){
+          if(isPrimeNumber(i)){
+              System.out.println(i);
+              }
+              }
+          }
+        public static boolean isPrimeNumber(int number){
+        for(int i = 2; i < number; i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
+
+
